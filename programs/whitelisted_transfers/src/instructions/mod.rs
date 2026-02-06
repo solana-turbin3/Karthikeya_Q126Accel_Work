c77d90cb0f76a2ac0350ee8334aa8259ce5c9546
KarthikeyaGundumogula <karthikeya200226@gmail.com>
2026-02-06 17:15:32 +0530

    cleaup

--- programs/whitelisted_transfers/src/instructions/mod.rs
+++
@@ -1,7 +0,0 @@
-pub mod init_extra_acc_meta;
-pub mod whitelist_operations;
-pub mod transfer_hook;
-
-pub use init_extra_acc_meta::*;
-pub use whitelist_operations::*;
-pub use transfer_hook::*;
--- programs/whitelisted_transfers/src/state/mod.rs
+++
@@ -1,6 +0,0 @@
-use anchor_lang::prelude::*;
-
-#[account]
-pub struct Status {
-    pub whitelist_status: bool,
-}
